1791083d27fa4a9de3adbbaa5640f1e1e1347cad
ilhan yilmaz <ilhannn@gmail.com>
2015-05-18 13:32:05 +0300

    dummy  commit

--- app/src/main/java/com/digitalwonders/ilhan/buddytalk/MainActivity.java
+++ app/src/main/java/com/digitalwonders/ilhan/buddytalk/MainActivity.java
@@ -1,17 +1,48 @@
 package com.digitalwonders.ilhan.buddytalk;
 
+import android.net.nsd.NsdServiceInfo;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    NsdHelper mNsdHelper;
+
+    private TextView mStatusView;
+    private Handler mUpdateHandler;
+
+    public static final String TAG = "NsdChat";
+
+    ChatConnection mConnection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        mStatusView = (TextView) findViewById(R.id.status);
+
+        mUpdateHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String chatLine = msg.getData().getString("msg");
+                addChatLine(chatLine);
+            }
+        };
+
+        mConnection = new ChatConnection(mUpdateHandler);
+
+        mNsdHelper = new NsdHelper(this);
+        mNsdHelper.initializeNsd();
     }
 
     @Override
@@ -34,5 +65,67 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void clickAdvertise(View v) {
+        // Register service
+        if(mConnection.getLocalPort() > -1) {
+            mNsdHelper.registerService(mConnection.getLocalPort());
+        } else {
+            Log.d(TAG, "ServerSocket isn't bound.");
+        }
+    }
+
+    public void clickDiscover(View v) {
+        mNsdHelper.discoverServices();
+    }
+
+    public void clickConnect(View v) {
+        NsdServiceInfo service = mNsdHelper.getChosenServiceInfo();
+        if (service != null) {
+            Log.d(TAG, "Connecting.");
+            mConnection.connectToServer(service.getHost(),
+                    service.getPort());
+        } else {
+            Log.d(TAG, "No service to connect to!");
+        }
+    }
+
+    public void clickSend(View v) {
+        EditText messageView = (EditText) this.findViewById(R.id.chatInput);
+        if (messageView != null) {
+            String messageString = messageView.getText().toString();
+            if (!messageString.isEmpty()) {
+                mConnection.sendMessage(messageString);
+            }
+            messageView.setText("");
+        }
+    }
+
+    public void addChatLine(String line) {
+        mStatusView.append("\n" + line);
+    }
+
+    @Override
+    protected void onPause() {
+        if (mNsdHelper != null) {
+            mNsdHelper.stopDiscovery();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mNsdHelper != null) {
+            mNsdHelper.discoverServices();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        mNsdHelper.tearDown();
+        mConnection.tearDown();
+        super.onDestroy();
     }
 }
